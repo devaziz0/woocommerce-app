0de97c8f7d7664de3e2bf52bf887066ca4678507
Aziz <a.attouche@esi-sba.dz>
2018-10-26 19:51:30 +0100

    Fix script

--- create_superuser.py
+++ create_superuser.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.db.utils import IntegrityError
-from woocommerceAppProject.woocommerceApp.models import Subscription
 email = 'admin@example.com'
 password = 'woocommerceadmin'
 
@@ -9,5 +8,4 @@ try:
     print ("User 'admin' already exist")
 except User.DoesNotExist:
     User.objects.create_superuser('admin', email, password)
-    Subscription.objects.create(name="Free Trial")
     print ("User 'admin created with default password'")
