876d09141ba0e7fa774f553c6dee949894e23a6b
Aziz <a.attouche@esi-sba.dz>
2018-10-26 22:50:17 +0100

    Added post to woocommerce url

--- woocommerceAppProject/woocommerceApp/urls.py
+++ woocommerceAppProject/woocommerceApp/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('timezone/', views.set_timezone, name="timezone"),
     path('dashboard/schedule/<int:id>/download/', views.get_csv_file, name="download"),
     path('dashboard/schedule/<int:id>/generate/', views.generate_csv_file, name="generate"),
+    path('dashboard/schedule/<int:id>/post/', views.woocommerce_post, name="post"),
     path('dashboard/schedule/', views.schedule_creation, name="schedule"),
     path('signup/',views.signup, name='signup'),
     path('accounts/', include('django.contrib.auth.urls'))
