c5409947f19c85ae5af99f5a8dfa5362b8a7064d
Aziz <a.attouche@esi-sba.dz>
2018-10-26 19:34:16 +0100

    Settings fix

--- woocommerceAppProject/woocommerceAppProject/settings.py
+++ woocommerceAppProject/woocommerceAppProject/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'woocommerceApp',
-    'django_q'
+    'woocommerceApp'
 ]
 
 MIDDLEWARE = [
@@ -75,25 +74,25 @@ WSGI_APPLICATION = 'woocommerceAppProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'woocommerce',
-#        'USER': 'woocommerce',
-#        'PASSWORD': 'woocommerce',
-#        'HOST': 'db',
-#        'PORT': 5432,
-#    }
-#
-# }
-
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-     }
- }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'woocommerce',
+       'USER': 'woocommerce',
+       'PASSWORD': 'woocommerce',
+       'HOST': 'db',
+       'PORT': 5432,
+   }
 
+}
+# 
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#      }
+#  }
+#
 
 
 # Password validation
