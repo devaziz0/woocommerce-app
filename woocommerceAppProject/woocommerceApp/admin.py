d1245935eaa857065e4749eab6dffedb668d0362
Aziz <a.attouche@esi-sba.dz>
2018-10-27 01:50:05 +0100

    Added subsciptions to admin panel

--- woocommerceAppProject/woocommerceApp/admin.py
+++ woocommerceAppProject/woocommerceApp/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 admin.site.register(Profil)
-admin.site.register(Subscription)
+admin.site.register(Free_Trial)
+admin.site.register(Basic_Subscription)
+admin.site.register(Premium_Subscription)
+admin.site.register(Gold_Subscription)
 admin.site.register(Schedule)
