bbfcdb1774a942a304e55028a798e16636bafd14
Aziz <a.attouche@esi-sba.dz>
2018-10-30 20:18:28 +0100

    Postgresql fix

--- woocommerceAppProject/woocommerceAppProject/settings.py
+++ woocommerceAppProject/woocommerceAppProject/settings.py
@@ -76,24 +76,24 @@ WSGI_APPLICATION = 'woocommerceAppProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'woocommerce',
-#        'USER': 'woocommerce',
-#        'PASSWORD': 'woocommerce',
-#        'HOST': 'db',
-#        'PORT': 5432,
-#    }
-#
-# }
-#
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-     }
- }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'woocommerce',
+       'USER': 'woocommerce',
+       'PASSWORD': 'woocommerce',
+       'HOST': 'db',
+       'PORT': 5432,
+   }
+
+}
+
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#      }
+#  }
 
 
 
