df66e573ad8fec84656ff68949d1821db6921fa2
Aziz <a.attouche@esi-sba.dz>
2018-10-30 19:49:10 +0100

    Fixed static routes

--- woocommerceAppProject/woocommerceAppProject/settings.py
+++ woocommerceAppProject/woocommerceAppProject/settings.py
@@ -76,25 +76,25 @@ WSGI_APPLICATION = 'woocommerceAppProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'woocommerce',
-       'USER': 'woocommerce',
-       'PASSWORD': 'woocommerce',
-       'HOST': 'db',
-       'PORT': 5432,
-   }
-
-}
-#
 # DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.sqlite3',
-#          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#      }
-#  }
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'woocommerce',
+#        'USER': 'woocommerce',
+#        'PASSWORD': 'woocommerce',
+#        'HOST': 'db',
+#        'PORT': 5432,
+#    }
 #
+# }
+#
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+ }
+
 
 
 # Password validation
@@ -134,6 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 LOGIN_REDIRECT_URL ='woocommerceApp:index'
 LOGOUT_REDIRECT_URL = 'woocommerceApp:index'
 
