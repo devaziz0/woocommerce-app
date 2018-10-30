82a49ba40c542e2a5150593850461936920fffce
Aziz <a.attouche@esi-sba.dz>
2018-10-30 19:49:21 +0100

    Fixed debug urls

--- woocommerceAppProject/woocommerceAppProject/urls.py
+++ woocommerceAppProject/woocommerceAppProject/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', include('woocommerceApp.urls')),
 
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
