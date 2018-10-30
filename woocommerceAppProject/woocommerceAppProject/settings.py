c5b89f8a66c20a32af123a1d860f8e52a30f6983
Aziz <a.attouche@esi-sba.dz>
2018-10-30 21:12:34 +0100

    Debug removal

--- woocommerceAppProject/woocommerceAppProject/settings.py
+++ woocommerceAppProject/woocommerceAppProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'uu+h!)!*&8s@h6avetqf_m%bj!b6#)v#$njz)tde04rp84i)uz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
